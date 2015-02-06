d6488930e44dd5ec9a0b5668d57c79900c6225d7
Yoshihiro Tanaka <feria.primavera@gmail.com>
2015-02-06 22:01:31 +0900

    add images, and update program.

--- programs/other/hist_plot.R
+++ programs/other/hist_plot.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-infile <- read.table("yri_overlap_forR.csv", sep=',', header=F)
+infile <- read.table("lwk_overlap_forR.csv", sep=',', header=F)
 max(infile$V2)
 
 summary(infile$V3)
@@ -18,4 +18,10 @@ values <- rbind(cps, pu)
 
 ggplot(values, aes(value, fill = legend)) + xlim(c(min(cps$value), max(pu$value))) + geom_density(alpha = 0.5)
 
-ggplot(values, aes(x = legend, y = value, fill = legend)) + geom_boxplot()
+box.plot = ggplot(values, aes(x = legend, y = value, fill = legend)) + geom_boxplot()
+
+# ref. http://d.hatena.ne.jp/aaikmyz/20130115/1358224472
+box.plot = box.plot + theme(axis.title.x = element_text(size=13),axis.title.y = element_text(size=13)) 
+box.plot = box.plot + theme(axis.text.x = element_text(size=15),axis.text.y = element_text(size=15)) 
+box.plot = box.plot + theme(legend.title = element_text(size=15),legend.text = element_text(size=15)) 
+box.plot
